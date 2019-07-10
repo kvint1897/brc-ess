6723f7eab51e67f41d3969395583b52cab8d7c57
salmi <salmi@kvint.md>
2019-07-10 12:04:55 +0300

    MVP implementation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,495 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <main>
+        <header>
+            <h1>
+                <span class="send" title="Отправить на сервер" @click="send">{{ count }}</span>
+                <span v-if="total>0" class="total">∑{{ total }}</span>
+            </h1>
+            <input ref="code" class="new-code"
+                   v-model="newCode"
+                   v-on:keyup.enter="addCode"
+                   placeholder="Просканируйте акцизную марку">
+        </header>
+        <section class="main">
+            <ul class="code-list">
+                <li v-for="(code, index) in filteredCodes"
+                    :key="index"
+                    :class="{ completed: code.completed, editing: code == editedCode}">
+                    <div class="view">
+                        <label @dblclick="editCode(code)">{{ code.title }}</label>
+                        <button class="destroy" @click="removeCode(code)"></button>
+                    </div>
+                    <input class="edit" type="text"
+                        v-model="code.title"
+                        @blur="doneEdit(code)"
+                        @keyup.enter="doneEdit(code)" />
+                </li>
+            </ul>
+            <ul class="code-list"></ul>
+        </section>
+    </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+const STORAGE_KEY = 'excise-codes'
+const API_URL = 'http://web.kvint.md/brc/'
+import axios from 'axios'
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    data() {
+        return {
+            newCode: '',
+            codes: [],
+            total: 0,
+            editedCode: null,
+            visibility: 'all'
+        }
+    },
+    created() {
+        let self = this;
+        window.addEventListener('keyup', function () {
+            self.$nextTick(() => {
+                self.$refs.code.focus()
+            })
+        }, true);
+        this.codes = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'); 
+    },
+    computed: {
+        count() {
+            return this.codes.length;
+        },
+        filteredCodes() {
+            if (this.visibility === 'all') {
+                return this.codes;
+            } else if (this.visibility === 'active') {
+                return this.codes.filter(function (code) {
+                    return !code.completed;
+                });
+            } else {
+                //completed
+                return this.codes.filter(function (code) {
+                    return code.completed;
+                })
+
+            }
+        }
+
+    },
+    methods: {
+        send() {
+            if(this.count === 0){
+                return
+            }
+            axios.post(API_URL, {
+                codes: this.codes
+            }, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+            .then(response => {
+                if (response.data.status == 'ok') {
+                    this.codes = []
+                    this.total = response.data.total
+                    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.codes))
+                }
+            })
+            .catch(e => {
+                /* eslint-disable no-console */
+                console.log(e)
+            });
+        },
+        addCode() {
+            var test = this.newCode.match(/[^-]+-([0-9]{11})[0-9]+?/);
+            this.newCode = '';
+            if (!test) {
+                return;
+            }
+            var val = test[1].trim(), uniq = true;
+            for (var i = 0, l = this.codes.length; i < l; i++) {
+                if (this.codes[i].title == val) {
+                    uniq = false;
+                    break;
+                }
+            }
+            if (!uniq) {
+                return;
+            }
+            this.codes.unshift({title: val, id: this.codes.length});
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(this.codes));
+
+        },
+        removeCode(code) {
+            this.codes.splice(this.codes.indexOf(code), 1);
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(this.codes));
+        },
+        editCode(code) {
+            this.editedCode = code;
+
+        },
+        doneEdit(code) {
+            if (!this.editedCode) {
+                return
+            }
+            this.editedCode = null;
+            code.title = code.title.trim();
+            if (!code.title) {
+                this.removeCode(code);
+            }
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(this.codes));
+        }
+    } 
 }
 </script>
+
+<style>
+	html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+button{
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
+  font-size: 100%;
+  vertical-align: baseline;
+  font-family: inherit;
+  font-weight: inherit;
+  color: inherit;
+  -webkit-appearance: none;
+  appearance: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+}
+
+body {
+  font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  line-height: 1.4em;
+  background: #f5f5f5;
+  color: #4d4d4d;
+  min-width: 230px;
+  max-width: 640px;
+  margin: 0 auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+  font-weight: 300;
+}
+
+button,
+input[type="checkbox"] {
+  outline: none;
+}
+
+.hidden {
+  display: none;
+}
+
+#app {
+  background: #fff;
+  margin: 200px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2),
+              0 25px 50px 0 rgba(0, 0, 0, 0.1);
+}
+
+#app input::-webkit-input-placeholder {
+  font-style: italic;
+  font-weight: 300;
+  color: #e6e6e6;
+}
+
+#app input::-moz-placeholder {
+  font-style: italic;
+  font-weight: 300;
+  color: #e6e6e6;
+}
+
+#app input::input-placeholder {
+  font-style: italic;
+  font-weight: 300;
+  color: #e6e6e6;
+}
+
+#app h1 {
+    
+  position: absolute;
+  top: -130px;
+  width: 100%;
+  font-size: 90px;
+  font-weight: 100;
+  text-align: center;
+  color: #000;
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+.send {
+    cursor: pointer;
+    float: left;
+}
+.total {
+    float: right;
+    color: green;
+}
+.new-code,
+.edit {
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+  border: 0;
+  outline: none;
+  color: inherit;
+  padding: 6px;
+  border: 1px solid #999;
+  box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+}
+
+.new-code {
+  padding: 16px 16px 16px 60px;
+  border: none;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
+}
+
+.main {
+  position: relative;
+  z-index: 2;
+  border-top: 1px solid #e6e6e6;
+}
+
+label[for='toggle-all'] {
+  display: none;
+}
+
+.toggle-all {
+  position: absolute;
+  top: -55px;
+  left: -12px;
+  width: 60px;
+  height: 34px;
+  text-align: center;
+  border: none;
+}
+
+.toggle-all:before {
+  content: 'вќЇ';
+  font-size: 22px;
+  color: #e6e6e6;
+  padding: 10px 27px 10px 27px;
+}
+
+.toggle-all:checked:before {
+  color: #737373;
+}
+
+.code-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.code-list li {
+  position: relative;
+  font-size: 24px;
+  border-bottom: 1px solid #ededed;
+}
+
+.code-list li:last-child {
+  border-bottom: none;
+}
+
+.code-list li.editing {
+  border-bottom: none;
+  padding: 0;
+}
+
+.code-list li.editing .edit {
+  display: block;
+  width: 506px;
+  padding: 13px 17px 12px 17px;
+  margin: 0 0 0 43px;
+}
+
+.code-list li.editing .view {
+  display: none;
+}
+
+/*.code-list li .toggle {
+  text-align: center;
+  width: 40px;
+  height: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  border: none;
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.code-list li .toggle:after {
+  content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="%23ededed" stroke-width="3"/></svg>');
+}
+
+.code-list li .toggle:checked:after {
+  content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="%23bddad5" stroke-width="3"/><path fill="%235dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>');
+}*/
+
+.code-list li label {
+  white-space: pre-line;
+  word-break: break-all;
+  padding: 15px 60px 15px 15px;
+  margin-left: 45px;
+  display: block;
+  line-height: 1.2;
+  transition: color 0.4s;
+}
+
+.code-list li.completed label {
+  color: #d9d9d9;
+  text-decoration: line-through;
+}
+
+.code-list li .destroy {
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  margin: auto 0;
+  font-size: 30px;
+  color: #cc9a9a;
+  margin-bottom: 11px;
+  transition: color 0.2s ease-out;
+}
+
+.code-list li .destroy:hover {
+  color: #af5b5e;
+}
+
+.code-list li .destroy:after {
+  content:  '\d7';
+}
+
+.code-list li:hover .destroy {
+  display: block;
+}
+
+.code-list li .edit {
+  display: none;
+}
+
+.code-list li.editing:last-child {
+  margin-bottom: -1px;
+}
+
+.footer {
+  color: #777;
+  padding: 10px 15px;
+  height: 20px;
+  text-align: center;
+  border-top: 1px solid #e6e6e6;
+}
+
+.footer:before {
+  content: '';
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 50px;
+  overflow: hidden;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2),
+              0 8px 0 -3px #f6f6f6,
+              0 9px 1px -3px rgba(0, 0, 0, 0.2),
+              0 16px 0 -6px #f6f6f6,
+              0 17px 2px -6px rgba(0, 0, 0, 0.2);
+}
+
+.code-count {
+  text-align: left;
+    color: #000;
+}
+
+.filters {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  position: absolute;
+  right: 0;
+  left: 0;
+}
+
+.filters li {
+  display: inline;
+}
+
+.filters li a {
+  color: inherit;
+  margin: 3px;
+  padding: 3px 7px;
+  text-decoration: none;
+  border: 1px solid transparent;
+  border-radius: 3px;
+}
+
+.filters li a.selected,
+.filters li a:hover {
+  border-color: rgba(175, 47, 47, 0.1);
+}
+
+.filters li a.selected {
+  border-color: rgba(175, 47, 47, 0.2);
+}
+
+.clear-completed,
+html .clear-completed:active {
+  float: right;
+  position: relative;
+  line-height: 20px;
+  text-decoration: none;
+  cursor: pointer;
+  position: relative;
+}
+
+.clear-completed:hover {
+  text-decoration: underline;
+}
+
+.info {
+  margin: 65px auto 0;
+  color: #bfbfbf;
+  font-size: 10px;
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+  text-align: center;
+}
+
+.info p {
+  line-height: 1;
+}
+
+.info a {
+  color: inherit;
+  text-decoration: none;
+  font-weight: 400;
+}
+
+.info a:hover {
+  text-decoration: underline;
+}
+</style>
